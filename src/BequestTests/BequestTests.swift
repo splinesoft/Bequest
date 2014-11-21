fb306c39d701a830e68c18218973ebc922892235
Jonathan Hersh <jon@her.sh>
2014-11-21 09:00:15 -0800

    Bequest tests are Quick and Nimble

--- src/BequestTests/BequestTests.swift
+++ src/BequestTests/BequestTests.swift
@@ -8,22 +8,17 @@
 
 import UIKit
 import XCTest
+import Quick
+import Nimble
 
-class BequestTests: XCTestCase {
+class BequestTests : QuickSpec {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func spec() {
+        describe("an HTTP client") {
+            it("is initializable") {
+                let client = BQSTHTTPClient()
+                expect(client).toNot(beNil())
+            }
+        }
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testHTTPClientInitializable() {
-        let client = BQSTHTTPClient()
-        XCTAssertNotNil(client, "HTTP Client should initialize")
-    }
-    
 }
