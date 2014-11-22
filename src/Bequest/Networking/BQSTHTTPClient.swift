c28626bbb0183eb9c89330b5fd7124e7b0dcb1cf
Jonathan Hersh <jon@her.sh>
2014-11-21 22:17:02 -0800

    simple HTTP client

--- src/Bequest/Networking/BQSTHTTPClient.swift
+++ src/Bequest/Networking/BQSTHTTPClient.swift
@@ -9,6 +9,42 @@
 import Foundation
 import Alamofire
 
-class BQSTHTTPClient {
+public typealias BQSTResponseBlock = (NSURLRequest, NSHTTPURLResponse?, AnyObject?, NSError?) -> Void
 
+public class BQSTHTTPClient {
+    
+    public class func requestForURL(url : NSURL,
+        method      : String,
+        headers     : [NSObject : AnyObject],
+        parameters  : [String:String]) -> NSURLRequest {
+            
+        let request = NSMutableURLRequest(URL: url)
+        
+        request.HTTPMethod = method
+        request.allHTTPHeaderFields = headers
+        
+        return request
+    }
+    
+    public class func request(url : NSURL, _ response : BQSTResponseBlock) {
+        
+        self.request(url, method: nil, headers: nil, parameters: nil, response)
+    }
+    
+    public class func request(url : NSURL, method : String, _ response : BQSTResponseBlock) {
+        
+        self.request(url, method: method, headers: nil, parameters: nil, response)
+    }
+    
+    public class func request(url: NSURL,
+        method: String?,
+        headers: [String:String]?,
+        parameters: [String:String]?,
+        _ response : BQSTResponseBlock) {
+            
+        let URLRequest = self.requestForURL(url, method: method ?? "GET", headers: headers ?? [:], parameters: parameters ?? [:])
+        
+        Alamofire.request(URLRequest).response(response)
+    }
+    
 }
--- src/BequestTests/BequestTests.swift
+++ src/BequestTests/BequestTests.swift
@@ -14,10 +14,49 @@ import Nimble
 class BequestTests : QuickSpec {
     
     override func spec() {
+        
         describe("an HTTP client") {
+            var URL : NSURL?
+            
+            beforeEach { URL = NSURL(string: "http://google.com")! }
+            
             it("is initializable") {
                 let client = BQSTHTTPClient()
                 expect(client).toNot(beNil())
+            }
+            
+            context("when creating NSURLRequest objects") {
+                
+                it("can create simple NSURLRequest objects") {
+                    let URLRequest : NSURLRequest = BQSTHTTPClient.requestForURL(URL!,
+                        method: "GET", headers: [:], parameters: [:]);
+                    
+                    expect(URLRequest).toNot(beNil())
+                    expect(URLRequest.URL.isEqual(URL)).to(beTruthy())
+                }
+                
+                it("can create a POST request") {
+                    let URLRequest : NSURLRequest = BQSTHTTPClient.requestForURL(URL!, method: "POST", headers: [:], parameters: [:])
+                    
+                    expect(URLRequest.HTTPMethod == "POST").to(beTruthy())
+                }
+            }
+            
+            context("when sending simple HTTP requests") {
+               
+                it("makes successful GET requests") {
+                    var receivedResponse = false
+                    
+                    BQSTHTTPClient.request(URL!) {
+                        (req, resp : NSHTTPURLResponse?, _, _) in
+                        
+                        receivedResponse = true
+                        expect(resp).toNot(beNil())
+                        expect(resp!.statusCode == 200).to(beTruthy())
+                    }
+                    
+                    expect(receivedResponse).toEventually(beTrue(), timeout: 30, pollInterval: 0.75)
+                }
             }
         }
     }
