44e45470e71270cd1a2c66c11d7c09b61f9931d1
Jonathan Hersh <jon@her.sh>
2014-11-23 22:01:06 -0800

    response struct serializes as a constructor

--- src/Bequest/Networking/BQSTHTTPClient.swift
+++ src/Bequest/Networking/BQSTHTTPClient.swift
@@ -9,6 +9,8 @@
 import Foundation
 import Alamofire
 
+// MARK: Response serializing
+
 public let BQSTHeaderContentType: String = "Content-Type"
 
 // More than we'll ever know what to do with: https://en.wikipedia.org/wiki/Internet_media_type
@@ -33,21 +35,78 @@ public enum BQSTResponseContentType: String, Printable {
     }
 }
 
-// An HTTP response
 public struct BQSTHTTPResponse {
     var contentType: BQSTResponseContentType? // The interpreted content type
     var object: AnyObject // The serialized object, or raw NSData if serializing failed
+    
+    public static func serializeResponse(response: NSHTTPURLResponse?, data: NSData) -> BQSTHTTPResponse {
+        var serializedResponse = BQSTHTTPResponse(contentType: nil, object: data)
+        
+        if let headers = response?.allHeaderFields {
+            
+            if let contentTypeRaw = headers[BQSTHeaderContentType] as? String {
+                
+                // e.g. Content-Type: "application/json; charset=ISO-8859-1"
+                let contentTypeComponents = contentTypeRaw.componentsSeparatedByString(";")
+                
+                if let contentType = BQSTResponseContentType(rawValue: contentTypeComponents.first as String!) {
+                    
+                    serializedResponse.contentType = contentType
+                    
+                    switch contentType {
+                    case .JSON:
+                        let JSONObject = BQSTJSONResult.resultForData(data, options: nil)
+                        
+                        switch JSONObject {
+                        case .Success(let dict):
+                            serializedResponse.object = dict
+                        default:
+                            break
+                        }
+                        
+                    case .XML:
+                        //                    let XMLParser = NSXMLParser(data: data)
+                        //
+                        //                    if XMLParser.parse() {
+                        //
+                        //                    }
+                        
+                        break
+                        
+                    case .GIF, .PNG, .JPEG:
+                        
+                        if let image = UIImage(data: data) {
+                            serializedResponse.object = image
+                        }
+                        
+                    case .TXT, .HTML:
+                        
+                        // TODO: charset
+                        
+                        serializedResponse.object = NSString(data: data, encoding: NSUTF8StringEncoding) ?? ""
+                        
+                    default:
+                        
+                        break
+                    }
+                }
+            }
+        }
+        
+        return serializedResponse
+    }
 }
 
 public typealias BQSTProgressBlock = (NSURLRequest, Float) -> Void
 public typealias BQSTResponseBlock = (NSURLRequest, NSHTTPURLResponse?, BQSTHTTPResponse?, NSError?) -> Void
+
+// MARK: JSON serializing
+
 public typealias BQSTJSONResponse = [NSObject:AnyObject]
 
 public enum BQSTJSONResult {
     case Success (BQSTJSONResponse)
     case Failure (NSError?)
-    
-    // MARK: JSON serializing
     
     public static func resultForData(data: NSData, options: NSJSONReadingOptions = .AllowFragments) -> BQSTJSONResult {
         
@@ -62,6 +121,8 @@ public enum BQSTJSONResult {
     }
 }
 
+// MARK: HTTP Client
+
 public class BQSTHTTPClient {
     
     // MARK: NSURLRequest helpers
@@ -69,7 +130,7 @@ public class BQSTHTTPClient {
     public class func requestForURL(url: NSURL,
         method: String = "GET",
         headers: [NSObject:AnyObject] = [:],
-        parameters: [String:String] = [:]) -> NSURLRequest {
+        parameters: [NSObject:AnyObject] = [:]) -> NSURLRequest {
             
         let request = NSMutableURLRequest(URL: url)
         
@@ -79,66 +140,6 @@ public class BQSTHTTPClient {
         // ...
         
         return request
-    }
-    
-    // MARK: Response serializing
-    
-    public class func serializeResponse(response: NSHTTPURLResponse?, data: NSData) -> BQSTHTTPResponse {
-        
-        var serializedResponse = BQSTHTTPResponse(contentType: nil, object: data)
-        
-        if let headers = response?.allHeaderFields {
-            
-            if let contentTypeRaw = headers[BQSTHeaderContentType] as? String {
-                
-                // e.g. Content-Type: "application/json; charset=ISO-8859-1"
-                let contentTypeComponents = contentTypeRaw.componentsSeparatedByString(";")
-                
-                if let contentType = BQSTResponseContentType(rawValue: contentTypeComponents.first as String!) {
-                    
-                    serializedResponse.contentType = contentType
-                
-                    switch contentType {
-                    case .JSON:
-                        let JSONObject = BQSTJSONResult.resultForData(data, options: nil)
-                        
-                        switch JSONObject {
-                        case .Success(let dict):
-                            serializedResponse.object = dict
-                        default:
-                            break
-                        }
-                        
-                    case .XML:
-    //                    let XMLParser = NSXMLParser(data: data)
-    //                    
-    //                    if XMLParser.parse() {
-    //                        
-    //                    }
-                        
-                        break
-                        
-                    case .GIF, .PNG, .JPEG:
-                        
-                        if let image = UIImage(data: data) {
-                            serializedResponse.object = image
-                        }
-                        
-                    case .TXT, .HTML:
-                    
-                        // TODO: charset
-                        
-                        serializedResponse.object = NSString(data: data, encoding: NSUTF8StringEncoding) ?? ""
-                    
-                    default:
-                        
-                        break
-                    }
-                }
-            }
-        }
-        
-        return serializedResponse
     }
     
     // MARK: HTTP Requests
@@ -180,7 +181,7 @@ public class BQSTHTTPClient {
             if let data = object as? NSData {
                 response(request,
                     URLResponse,
-                    self.serializeResponse(URLResponse, data: data),
+                    BQSTHTTPResponse.serializeResponse(URLResponse, data: data),
                     error)
             } else {
                 response(URLRequest, URLResponse, nil, error)
