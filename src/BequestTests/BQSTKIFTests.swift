34f8dd3e973b7224d40c9f816a6a0d5c39a8bd75
Jonathan Hersh <jon@her.sh>
2014-12-25 21:20:18 -0800

    update tests

--- src/BequestTests/BQSTKIFTests.swift
+++ src/BequestTests/BQSTKIFTests.swift
@@ -12,13 +12,15 @@ import KIF
 
 class BQSTKIFTests : KIFTestCase {
     
-    private func tester() -> KIFUITestActor {
-        return KIFUITestActor(inFile: __FILE__, atLine: __LINE__, delegate: self)
+    private var tester: KIFUITestActor {
+        get {
+            return KIFUITestActor(inFile: __FILE__, atLine: __LINE__, delegate: self)
+        }
     }
     
     private func setUpRequest(url: String, method: String) {
-        tester().clearTextFromAndThenEnterText(url, intoViewWithAccessibilityLabel: "URL")
-        tester().clearTextFromAndThenEnterText(method, intoViewWithAccessibilityLabel: "Method")
+        tester.clearTextFromAndThenEnterText(url, intoViewWithAccessibilityLabel: "URL")
+        tester.clearTextFromAndThenEnterText(method, intoViewWithAccessibilityLabel: "Method")
     }
     
     override func beforeAll() {
@@ -32,44 +34,44 @@ class BQSTKIFTests : KIFTestCase {
     
     func testEmptyMethod() {
         self.setUpRequest("http://splinesoft.net", method: "")
-        tester().tapViewWithAccessibilityLabel("Send")
-        tester().tapViewWithAccessibilityLabel("Darn")
+        tester.tapViewWithAccessibilityLabel("Send")
+        tester.tapViewWithAccessibilityLabel("Darn")
     }
     
     func testEmptyURL() {
         self.setUpRequest("", method: "GET")
-        tester().tapViewWithAccessibilityLabel("Send")
-        tester().tapViewWithAccessibilityLabel("Darn")
+        tester.tapViewWithAccessibilityLabel("Send")
+        tester.tapViewWithAccessibilityLabel("Darn")
     }
     
     func testSimpleRequest() {
         self.setUpRequest("http://httpbin.org/get", method: "GET")
-        tester().tapViewWithAccessibilityLabel("Send")
-        tester().waitForViewWithAccessibilityLabel("Response [200]")
-        tester().tapViewWithAccessibilityLabel("Back")
+        tester.tapViewWithAccessibilityLabel("Send")
+        tester.waitForViewWithAccessibilityLabel("Response [200]")
+        tester.tapViewWithAccessibilityLabel("Back")
     }
     
     func testCustomURLAndMethod() {
         self.setUpRequest("http://httpbin.org/post", method: "POST")
-        tester().tapViewWithAccessibilityLabel("Send")
-        tester().waitForViewWithAccessibilityLabel("Response [200]")
-        tester().tapViewWithAccessibilityLabel("Back")
+        tester.tapViewWithAccessibilityLabel("Send")
+        tester.waitForViewWithAccessibilityLabel("Response [200]")
+        tester.tapViewWithAccessibilityLabel("Back")
     }
     
     func testCustomURLAndInvalidMethod() {
         self.setUpRequest("http://httpbin.org/post", method: "NOT-POST")
-        tester().tapViewWithAccessibilityLabel("Send")
-        tester().waitForViewWithAccessibilityLabel("Response [405]")
-        tester().tapViewWithAccessibilityLabel("Back")
+        tester.tapViewWithAccessibilityLabel("Send")
+        tester.waitForViewWithAccessibilityLabel("Response [405]")
+        tester.tapViewWithAccessibilityLabel("Back")
     }
     
     func testRefreshingResponse() {
         self.setUpRequest("https://google.com", method: "GET")
-        tester().tapViewWithAccessibilityLabel("Send")
-        tester().waitForViewWithAccessibilityLabel("Response [200]")
-        tester().swipeViewWithAccessibilityLabel("Response", inDirection: .Down)
-        tester().swipeViewWithAccessibilityLabel("Response", inDirection: .Down)
-        tester().waitForAbsenceOfViewWithAccessibilityLabel("Refresh")
-        tester().tapViewWithAccessibilityLabel("Back")
+        tester.tapViewWithAccessibilityLabel("Send")
+        tester.waitForViewWithAccessibilityLabel("Response [200]")
+        tester.swipeViewWithAccessibilityLabel("Response", inDirection: .Down)
+        tester.swipeViewWithAccessibilityLabel("Response", inDirection: .Down)
+        tester.waitForAbsenceOfViewWithAccessibilityLabel("Refresh")
+        tester.tapViewWithAccessibilityLabel("Back")
     }
 }
